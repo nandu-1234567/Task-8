9c8634ab81cee3faea4fa01b8c8ae5364b4592e9
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-19 17:19:35 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -123,7 +123,7 @@ resource "aws_ecs_task_definition" "strapi" {
 ########################
 
 resource "aws_ecs_service" "strapi_service" {
-  name            = "strapi-service1"
+  name            = "strapi-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.strapi.arn
   desired_count   = 1
