0b781ab10720eb24548c71ba08570970d6aae585
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 13:42:50 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,32 +1,13 @@
 data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.1.0.0/16"
+
+# Reference existing default VPC
+data "aws_vpc" "existing" {
+  default = true
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+# Reference all subnets in the VPC
+data "aws_subnet_ids" "public" {
+  vpc_id = data.aws_vpc.existing.id
 }
 
-resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.${count.index}.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
-}
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route" "public_internet_access" {
-  route_table_id         = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "public_assoc" {
-  count          = 2
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public_rt.id
-}
