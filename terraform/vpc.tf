505f1f81fcb41ed5939dcd00ad4768e835bb77fc
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 14:21:13 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {}
 data "aws_vpc" "default" {
   default = true
 }
-data "aws_subnets" "public" {
-    values = data.aws_vpc.default.id
+data "aws_subnet" "public" {
+    value = data.aws_vpc.default.id
   }
 
