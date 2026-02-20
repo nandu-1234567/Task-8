8c621243d7829239120037be1a16645e1a2e6a2a
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 14:34:39 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,6 +4,9 @@ data "aws_vpc" "default" {
   default = true
 }
 data "aws_subnets" "public" {
-    value = data.aws_vpc.default.id
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
+}
 
