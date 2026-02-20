88741ff1b9ece04fd73badbfd3763ed5509bfa98
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 11:20:03 +0530

    Create ecs.tf

---
+++ terraform/ecs.tf
@@ -0,0 +1,53 @@
+resource "aws_ecs_cluster" "main" {
+  name = var.cluster_name
+}
+
+resource "aws_ecs_task_definition" "strapi_task" {
+  family                   = var.task_family
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.cpu
+  memory                   = var.memory
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "strapi"
+      image     = "${aws_ecr_repository.strapi_repo.repository_url}:latest"
+      essential = true
+      portMappings = [{
+        containerPort = 1337
+      }]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.strapi_logs.name
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs/strapi"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "strapi_service" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.strapi_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = aws_subnet.public[*].id
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.strapi_tg.arn
+    container_name   = "strapi"
+    container_port   = 1337
+  }
+
+  depends_on = [aws_lb_listener.listener]
+}
