44f32573f6f0ecd7a30bafb62d420d90b09341c1
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 11:41:09 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "strapi-terraform-state-bucket"
     key            = "strapi/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
