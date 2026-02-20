026fd74a43e80cd48a994d7edc1c73d2bb87eb4b
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 11:15:14 +0530

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,17 @@
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+}
