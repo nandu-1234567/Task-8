4fcbac7589c809d5d6c548d541eee42139583a0c
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 11:22:37 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,14 +1,27 @@
-variable "aws_region" {
-  type    = string
+variable "region" {
   default = "us-east-1"
 }
 
-variable "ecr_image_uri" {
-  type    = string
-  default = "811738710312.dkr.ecr.us-east-1.amazonaws.com/strapi:latest"
+variable "cluster_name" {
+  default = "strapi-cluster"
 }
 
-variable "app_port" {
-  type    = number
-  default = 1337
+variable "service_name" {
+  default = "strapi-service"
+}
+
+variable "task_family" {
+  default = "strapi-task"
+}
+
+variable "cpu" {
+  default = "512"
+}
+
+variable "memory" {
+  default = "1024"
+}
+
+variable "log_group_name" {
+  default = "/ecs/strapi"
 }
