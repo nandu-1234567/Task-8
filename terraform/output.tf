191d0f4344869179706d305a6beca0c25f760ee1
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 13:04:32 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
 output "application_url" {
   value = "http://${aws_lb.alb.dns_name}"
 }
+output "ecs_task_execution_role_arn" {
+  value       = var.ecs_task_execution_role_arn
+  description = "ARN of ECS Task Execution Role for Strapi"
+}
