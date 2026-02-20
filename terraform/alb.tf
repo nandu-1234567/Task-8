5faa4753897b1f95be1b3338aa6d48b74b7487ae
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 14:17:35 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -9,7 +9,7 @@ resource "aws_lb" "alb" {
 resource "aws_lb_target_group" "strapi_tg" {
   port        = 1337
   protocol    = "HTTP"
-  vpc_id      = data.aws_vpc.existing.id
+  vpc_id      = data.aws_vpc.default.id
   target_type = "ip"
 }
 
