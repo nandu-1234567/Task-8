90dd1021386b62cd1c2f42d9566a2a42a00a91dd
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 13:58:02 +0530

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "alb_sg" {
-  name = "alb-sg1"
+  name = "alb-sg"
   vpc_id = data.aws_vpc.existing.id
 
   ingress {
@@ -17,20 +17,3 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-resource "aws_security_group" "ecs_sg" {
-  vpc_id =  data.aws_vpc.existing.id
-
-  ingress {
-    from_port       = 1337
-    to_port         = 1337
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
