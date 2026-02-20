dd772ac217fb1e9c57bee34a10bf440924f8beef
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 14:36:10 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -38,7 +38,7 @@ resource "aws_ecs_service" "strapi_service" {
   desired_count   = 1
 
   network_configuration {
-    subnets          = data.aws_subnet_ids.public.ids
+    subnets          = data.aws_subnet.public.ids
     security_groups  = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
   }
