c20b52fee838023a7dd5fbd9af256626b9978379
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 14:32:28 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "alb" {
   name               = "strapi-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            =  data.aws_subnet_ids.public.ids
+  subnets            =  data.aws_subnets.public.ids
   security_groups    = [aws_security_group.alb_sg.id]
 }
 
