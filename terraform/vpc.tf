d379cbe4b9da6e6f2ae2e37903958ae5eb713c65
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-20 14:48:18 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,17 +1,13 @@
 # Get all availability zones in the current region
 data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "strapi-vpc"
-  }
+data "aws_vpc" "existing" {
+  id = "vpc-0bc0b17365646bf3b"  # Pick one of your existing VPCs
 }
 
-resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.${count.index}.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
 }
